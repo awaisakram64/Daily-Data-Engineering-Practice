7b811d61a410650aabe94fc2eaeed486cdf75a2b
Awais Akram <awaisakram64@gmail.com>
2025-02-20 02:23:18 +0500

    Have you ever used recursive queries in SQL before? Share your experience or try tweaking the snippet with your dataset!

---
+++ daily_sql_dataengineering_tips/master_sqls_magic_with_recursive_queries_.py
@@ -0,0 +1,1 @@
+WITH RECURSIVE EmployeeHierarchy AS ( SELECT employee_id, name, manager_id FROM employees WHERE manager_id IS NULL UNION ALL SELECT e.employee_id, e.name, e.manager_id FROM employees e INNER JOIN EmployeeHierarchy eh ON e.manager_id = eh.employee_id ) SELECT name FROM EmployeeHierarchy;
