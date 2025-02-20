e0b4449fe35f0070e47af2180adb6b76ccbd4638
Awais Akram <awaisakram64@gmail.com>
2025-02-20 11:26:45 +0500

    Have you used SQL EXPLAIN plans before? Share your experiences or challenges with optimizing SQL queries!

---
+++ daily_sql_dataengineering_tips/harness_sqls_potential_with_explain_plans_.py
@@ -0,0 +1,3 @@
+-- Analyze your query performance with an EXPLAIN plan
+-- Here's an example query you need to investigate:
+EXPLAIN SELECT employee_id, first_name FROM employees WHERE department_id = 10;
