eb3e4591bd529208926d7a893a044d77b636455b
Awais Akram <awaisakram64@gmail.com>
2025-02-20 06:17:53 +0500

    Have you ever used recursive CTEs to solve a problem? Share your experience or give this challenge a try!

---
+++ daily_sql_dataengineering_tips/sql_with_recursive_hierarchy_unveiled_.py
@@ -0,0 +1,1 @@
+WITH RECURSIVE EmployeeHierarchy AS ( SELECT employee_id, manager_id, employee_name, 1 AS level FROM employees WHERE manager_id IS NULL UNION ALL SELECT e.employee_id, e.manager_id, e.employee_name, eh.level + 1 FROM employees e INNER JOIN EmployeeHierarchy eh ON e.manager_id = eh.employee_id ) SELECT * FROM EmployeeHierarchy ORDER BY level, employee_id;
